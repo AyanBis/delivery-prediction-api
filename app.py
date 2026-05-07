9b23f7d72079b6a4e9efb161759a965820ec3f4a
AyanBis <biswasayan418@gmail.com>
2026-05-08 00:03:56 +0530

    Fixed Render deployment

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
 from flask import Flask, request, jsonify
-import pickle
 
 app = Flask(__name__)
 
-class DeliveryModel:
-    
-    def predict(self, distance, weight):
-        return 0.5 + (distance * 0.2) + (weight * 0.1)
-    
-with open("delivery_model.pkl", "rb") as file:
-    model = pickle.load(file)
-    
 @app.route("/")
 def home():
     return "Flask Delivery Prediction API Is Running!"
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    
+
     data = request.get_json()
-    
+
     distance = data["distance"]
     weight = data["weight"]
-    
-    result = model.predict(distance, weight)
-    
+
+    result = 0.5 + (distance * 0.2) + (weight * 0.1)
+
     return jsonify({
         "estimated_delivery_time": result
     })
